2909806a1ec49d8ee63a3a41bd66f34db3c112b2
Thierry Walrant <git.tgc@walrant.net>
2009-11-05 19:13:11 +0100

    Add copyright files

--- OutlookGnuPG/About.cs
+++ OutlookGnuPG/About.cs
@@ -42,7 +42,7 @@ namespace OutlookGnuPG
       IconLabel.Links.Add(0, IconLabel.Text.Length, "http://www.famfamfam.com/");
       OpenPGPLink.Links.Add(0, OpenPGPLink.Text.Length, "http://www.starksoft.com/");
       ClipboardLink.Links.Add(0, ClipboardLink.Text.Length, "http://www.codeproject.com/KB/system/clipboard_backup_cs.aspx?display=Print");
-      ForkLabel.Links.Add(0, ForkLabel.Text.Length, "http://wiki.yobi.be/wiki/OutlookGnuPG");
+      ForkLabel.Links.Add(0, ForkLabel.Text.Length, "http://github.com/twalrant/OutlookGnuPG");
 
       Text = String.Format("About {0} ", AssemblyTitle);
 
